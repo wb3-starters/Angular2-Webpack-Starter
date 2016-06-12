6758c07b174855c7dcdde27210d3b3a82506b4d1
Bill Bensing <bill@billbensing.com>
2016-06-11 22:29:36 -0400

    properly implemented form validation for new account feature

--- src/component/new-account/new-account.component.ts
+++ src/component/new-account/new-account.component.ts
@@ -1,30 +1,49 @@
 import { Component } from '@angular/core';
+import {
+  FORM_DIRECTIVES,
+  FormBuilder,
+  ControlGroup,
+  Validators,
+  Control
+} from '@angular/common';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { LandingComponent } from '../../component';
 
 @Component({
   selector: 'new-account',
   template: require('./new-account.component.html'),
-  styleUrls:[require('./new-account.component.css')],
-  directives: [ ROUTER_DIRECTIVES ]
+  styleUrls: [require('./new-account.component.css')],
+  directives: [ FORM_DIRECTIVES ]
 })
 
+export class NewAccountComponent {
 
-export class NewAccountComponent {   
+  newAccountForm: ControlGroup;
+
+  firstNameControl: Control;
+  lastNameControl: Control;
+  emailControl: Control;
+
   constructor(
-    private _router: Router) {}
-   
+    private _router: Router,
+    private _formBuilder: FormBuilder) {
+
+    this.firstNameControl = new Control('', Validators.required);
+    this.lastNameControl = new Control('', Validators.required);
+    this.emailControl = new Control('', Validators.required);
+
+    this.newAccountForm = _formBuilder.group({
+      firstName: this.firstNameControl,
+      lastName: this.lastNameControl,
+      email: this.emailControl
+    });
+
+  }
+
   title = "Create a New Account";
 
-  user = {
-      firstName: "", 
-      lastName: "",
-      email: ""
-  };
-  
-  CreateAccount(user: any)
-  { 
-      console.log(user);
-      this._router.navigateByUrl('/new-account/confirmation');
+  onCreateAccount(user: any) {
+    console.log(user);
+    this._router.navigateByUrl('/new-account/confirmation');
   }
 }
